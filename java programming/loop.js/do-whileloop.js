71886af7b42b68449c3f4733e9f6ace1f544687f
ankit mor <ankitmor1811@gmail.com>
2026-02-27 14:40:45 +0530

    Add files via upload

---
+++ java programming/loop.js/do-whileloop.js
@@ -0,0 +1,6 @@
+let i=10;
+
+do{
+    console.log(i);  // do while loop:-
+    i++;
+}while(i<=15);
---
+++ java programming/loop.js/forloop.js
@@ -0,0 +1,5 @@
+let nm=0;
+
+for(let i=0;i<=100;i++){       // for loop:-
+    console.log(i);
+}
