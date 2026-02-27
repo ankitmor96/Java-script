4b0e6cc81607ee2e624d954a657880a8e6dcf0cd
ankit mor <ankitmor1811@gmail.com>
2026-02-27 14:35:57 +0530

    Add files via upload

---
+++ java programming/ConditionalStatments.js
@@ -0,0 +1,64 @@
+// if -------------
+
+let age = 18;
+
+if (age>=18)
+    console.log("you agr is Adult",age);
+
+// if-else -----------
+
+var Age = 16;
+
+if (Age>=18){
+    console.log("you age is Adult , can apply licence");
+}else{
+    console.log("you age is minor , can not  apply licence");
+}
+
+// Else-if ---------
+
+let mark=33;
+
+if(mark >=90){
+    console.log("grade : A");
+    console.log("you are pass smart and  good your work ");
+}else if(mark>=70){
+    console.log("grade : B");
+    console.log("you are pass very good your work ");
+}else if(mark>=50){
+    console.log("grade : C");
+    console.log("you are pass good your work ");
+}else if(mark>=35){
+    console.log("grade : D");
+    console.log("you are pass ");
+}else{
+    console.log("you are failed");
+}
+
+// switch case ----------
+
+
+let n=0;
+
+switch(mark<=50){
+
+    case 1:
+    console.log("grade A");
+    break;
+
+    case 2:
+    console.log("grade B"); // switch case :-
+    break;
+
+    case 3:
+    console.log("grade C");
+    break;
+
+    case 4:
+    console.log("grade D");
+    break;
+
+default:
+    console.log("you are failed: try once ") ; 
+
+}
