bfa0475b1a944a2658793abc64ecdade86f60f0f
ankit mor <ankitmor1811@gmail.com>
2026-03-13 11:30:02 +0530

    Add files via upload

---
+++ java programming/modules/export.js
@@ -0,0 +1,20 @@
+// export default :-
+
+// export default function greet(name){
+
+// return`ankit, ${name}`;
+
+// }
+
+
+
+// export using multiple sum :-
+
+export  const  sum = (a,b) => {
+
+  console.log(a + b) ;
+  console.log(a - b) ;
+  console.log(a * b) ;
+  console.log(a / b) ;
+
+}
---
+++ java programming/modules/import.js
@@ -0,0 +1,13 @@
+// import using no 
+
+// import greet from "./export.js";  
+ 
+// console.log(greet('mor'));
+
+
+
+// import usimg multiple sum :-
+
+import {sum}  from "./export.js";
+
+console.log(sum(8,2));
